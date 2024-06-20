d391397e7b0f871b4372b39c613864ae9c603c33
Srikanth Mattigunta <srikanth_mattigunta@epam.com>
2024-06-20 15:26:37 +0530

    newscript file alomg with few commands are pushed to git

---
+++ jenkinscripts/editionalscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# editional script execution.
+echo "Starting shell script execution..."
+echo "Current directory: $(pwd)"
+echo "Executing some commands..."
+echo "Command 1 output: $(ls -l)"
+echo "Command 2 output: $(uname -a)"
